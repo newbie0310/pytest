9aa443c5837f86d3803ea814cedaa438a2205557
john96@126.com <john96@126.com>
2020-08-28 00:54:55 +0800

    Signed-off-by: john96@126.com <john96@126.com>

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
-import tensorflow as tf
-tf.app.flags.DEFINE_string('gpus', None, 'gpus to use')
-tf.app.flags.DEFINE_integer('batch_size', 5, 'batch size')
-
-FLAGS = tf.app.flags.FLAGS
-
-def main(_):
-    print(FLAGS.gpus)
-    print(FLAGS.batch_size)
-
-if __name__=="__main__":
-    tf.app.run()
+from sys import argv
+ 
+script, first, second, third = argv
+ 
+print "The script is called:", script
+print "Your first variavle is:", first
+print "Your second variavle is:", second
+print "Your third variavle is:", third
